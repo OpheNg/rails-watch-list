70d41c5770fd9e0f221cefac17a37e08d8fe572b
OpheNg <ophe.ng@live.fr>
2022-07-23 15:52:35 +0200

    galerrrrre

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :list
+  resources :lists
+  resources :bookmarks
+  resources :movies
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
